622f45ce2b1d1919748a732e2279d38c6147b817
Bruno Henrique <brunohs.dev@gmail.com>
2018-10-17 16:52:43 -0300

    ajustes

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 
-const htmlPlugin = new HtmlWebPackPlugin({
-  template: "./src/index.html",
+const htmlWebpackPlugin = new HtmlWebPackPlugin({
+  template: "./src/app.html",
   filename: "./index.html"
 });
 
@@ -14,8 +14,26 @@ module.exports = {
         use: {
           loader: "babel-loader"
         }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              importLoaders: 1,
+              localIdentName: "[name]_[local]_[hash:base64]",
+              sourceMap: true,
+              minimize: true
+            }
+          }
+        ]
       }
     ]
   },
-  plugins: [htmlPlugin]
+  plugins: [htmlWebpackPlugin]
 };
